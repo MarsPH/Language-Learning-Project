0295c446f13950be9e17c72934fc5b67a020f7a4
AegonSnowX <156529617+AegonSnowX@users.noreply.github.com>
2024-04-15 18:18:39 -0400

    a

--- Project-Maqsad/Program.cs
+++ Project-Maqsad/Program.cs
@@ -11,10 +11,9 @@ static class Program
         // To customize application configuration such as set high DPI settings or default font,
         // see https://aka.ms/applicationconfiguration.
         ApplicationConfiguration.Initialize();
-<<<<<<< HEAD
-        Application.Run(new game1());
-=======
-        Application.Run(new Game2());
->>>>>>> 4e7d2568fdfa507628d193f48934165d6ab1e94b
+
+        Application.Run(new menu());
+
     }    
+        
 }
